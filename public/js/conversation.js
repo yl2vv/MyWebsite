9673072651fa4365720050bb0dfa3443bed56297
yl2vv <46683186+yl2vv@users.noreply.github.com>
2020-08-08 23:35:01 -0400

    Add files via upload

---
+++ public/js/conversation.js
@@ -0,0 +1,246 @@
+// The ConversationPanel module is designed to handle
+// all display and behaviors of the conversation column of the app.
+/* eslint no-unused-vars: "off" */
+/* global Api: true, Common: true*/
+
+var ConversationPanel = (function () {
+  var settings = {
+    selectors: {
+      chatBox: '#scrollingChat',
+      fromUser: '.from-user',
+      fromWatson: '.from-watson',
+      latest: '.latest'
+    },
+    authorTypes: {
+      user: 'user',
+      watson: 'watson'
+    }
+  };
+
+  // Publicly accessible methods defined
+  return {
+    init: init,
+    inputKeyDown: inputKeyDown,
+    sendMessage: sendMessage
+  };
+
+  // Initialize the module
+  function init() {
+    chatUpdateSetup();
+    Api.getSessionId(function() {
+      Api.sendRequest('', null);
+    });
+  }
+  // Set up callbacks on payload setters in Api module
+  // This causes the displayMessage function to be called when messages are sent / received
+  function chatUpdateSetup() {
+    var currentRequestPayloadSetter = Api.setRequestPayload;
+    Api.setRequestPayload = function (newPayloadStr) {
+      currentRequestPayloadSetter.call(Api, newPayloadStr);
+      displayMessage(JSON.parse(newPayloadStr), settings.authorTypes.user);
+    };
+
+    var currentResponsePayloadSetter = Api.setResponsePayload;
+    Api.setResponsePayload = function (newPayloadStr) {
+      currentResponsePayloadSetter.call(Api, newPayloadStr);
+      displayMessage(JSON.parse(newPayloadStr).result, settings.authorTypes.watson);
+    };
+
+    Api.setErrorPayload = function (newPayload) {
+      displayMessage(newPayload, settings.authorTypes.watson);
+    };
+  }
+
+
+  // Display a user or Watson message that has just been sent/received
+  function displayMessage(newPayload, typeValue) {
+    var isUser = isUserMessage(typeValue);
+    //var textExists = newPayload.generic;
+    if ((newPayload.output && newPayload.output.generic) ||  newPayload.input){
+      // Create new message generic elements
+      var responses = buildMessageDomElements(newPayload, isUser);
+      var chatBoxElement = document.querySelector(settings.selectors.chatBox);
+      var previousLatest = chatBoxElement.querySelectorAll((isUser ? settings.selectors.fromUser : settings.selectors.fromWatson) +
+        settings.selectors.latest);
+      // Previous "latest" message is no longer the most recent
+      if (previousLatest) {
+        Common.listForEach(previousLatest, function (element) {
+          element.classList.remove('latest');
+        });
+      }
+      if (! isUser) {
+        setResponse(responses, chatBoxElement, 0, true);
+      }
+    }
+  }
+
+  // Recurisive function to add responses to the chat area
+  function setResponse(responses, chatBoxElement, index, isTop) {
+    if (index < responses.length) {
+      var res = responses[index];
+      if (res.type !== 'pause') {
+        var currentDiv = getDivObject(res);
+        if (chatBoxElement.childElementCount > 0) {
+          chatBoxElement.removeChild(chatBoxElement.firstChild)
+        }
+        chatBoxElement.appendChild(currentDiv);
+        // Class to start fade in animation
+        currentDiv.classList.add('load');
+        // Move chat to the most recent messages when new messages are added
+        setResponse(responses, chatBoxElement, index + 1, false);
+      } else {
+        var userTypringField = document.getElementById('user-typing-field');
+        if (res.typing) {
+          userTypringField.innerHTML = 'Watson Assistant Typing...';
+        }
+        setTimeout(function () {
+          userTypringField.innerHTML = '';
+          setResponse(responses, chatBoxElement, index + 1, isTop);
+        }, res.time);
+      }
+    }
+  }
+
+  // Constructs new DOM element from a message
+  function getDivObject(res) {
+    var classes = ['from-watson', 'latest'];
+    var messageJson = {
+      'tagName': 'p',
+      'text': res.innerhtml
+    };
+    return Common.buildDomElement(messageJson);
+  }
+
+  // Checks if the given typeValue matches with the user "name", the Watson "name", or neither
+  // Returns true if user, false if Watson, and null if neither
+  // Used to keep track of whether a message was from the user or Watson
+  function isUserMessage(typeValue) {
+    if (typeValue === settings.authorTypes.user) {
+      return true;
+    } else if (typeValue === settings.authorTypes.watson) {
+      return false;
+    }
+    return null;
+  }
+
+  function getOptions(optionsList, preference) {
+    var list = '';
+    var i = 0;
+    if (optionsList !== null) {
+      if (preference === 'text') {
+        list = '<ul>';
+        for (i = 0; i < optionsList.length; i++) {
+          if (optionsList[i].value) {
+            list += '<li><div class="options-list" onclick="ConversationPanel.sendMessage(\'' +
+            optionsList[i].value.input.text + '\');" >' + optionsList[i].label + '</div></li>';
+          }
+        }
+        list += '</ul>';
+      } else if (preference === 'button') {
+        list = '<br>';
+        for (i = 0; i < optionsList.length; i++) {
+          if (optionsList[i].value) {
+            var item = '<div class="options-button" onclick="ConversationPanel.sendMessage(\'' +
+              optionsList[i].value.input.text + '\');" >' + optionsList[i].label + '</div>';
+            list += item;
+          }
+        }
+      }
+    }
+    return list;
+  }
+
+  function getResponse(responses, gen) {
+    var title = '', description = '';
+    if (gen.hasOwnProperty('title')) {
+      title = gen.title;
+    }
+    if (gen.hasOwnProperty('description')) {
+      description = '<div>' + gen.description + '</div>';
+    }
+    if (gen.response_type === 'image') {
+      var img = '<div><img src="' + gen.source + '" width="300"></div>';
+      responses.push({
+        type: gen.response_type,
+        innerhtml: title + description + img
+      });
+    } else if (gen.response_type === 'text') {
+      responses.push({
+        type: gen.response_type,
+        innerhtml: gen.text
+      });
+    } else if (gen.response_type === 'pause') {
+      responses.push({
+        type: gen.response_type,
+        time: gen.time,
+        typing: gen.typing
+      });
+    } else if (gen.response_type === 'option') {
+      var preference = 'text';
+      if (gen.hasOwnProperty('preference')) {
+        preference = gen.preference;
+      }
+
+      var list = getOptions(gen.options, preference);
+      responses.push({
+        type: gen.response_type,
+        innerhtml: title + description + list
+      });
+    }
+  }
+
+  // Constructs new generic elements from a message payload
+  function buildMessageDomElements(newPayload, isUser) {
+    var textArray = isUser ? newPayload.input.text : newPayload.output.text;
+    if (Object.prototype.toString.call(textArray) !== '[object Array]') {
+      textArray = [textArray];
+    }
+
+    var responses = [];
+
+    if (newPayload.hasOwnProperty('output')) {
+      if (newPayload.output.hasOwnProperty('generic')) {
+
+        var generic = newPayload.output.generic;
+
+        generic.forEach(function (gen) {
+          getResponse(responses, gen);
+        });
+      }
+    } else if (newPayload.hasOwnProperty('input')) {
+      var input = '';
+      textArray.forEach(function (msg) {
+        input += msg + ' ';
+      });
+      input = input.trim()
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+
+      if (input.length !== 0) {
+        responses.push({
+          type: 'text',
+          innerhtml: input
+        });
+      }
+    }
+    return responses;
+  }
+
+
+  function sendMessage(text) {
+    // Send the user message
+    Api.sendRequest(text);
+  }
+
+  // Handles the submission of input
+  function inputKeyDown(event, inputBox) {
+    // Submit on enter key, dis-allowing blank messages
+    if (event.keyCode === 13 && inputBox.value) {
+      sendMessage(inputBox.value);
+      // Clear input box for further messages
+      inputBox.value = '';
+      Common.fireEvent(inputBox, 'input');
+    }
+  }
+}());
